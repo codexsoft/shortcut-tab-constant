19bebf08789a588833a955171202ee334b9bdd6b
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2019-12-11 02:47:14 +0700

    initial commit

---
+++ src/TabConstant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CodexSoft\Shortcut;
+
+class TabConstant
+{
+
+    public static function register(): void
+    {
+        // do nothing but inits class loading with functions
+    }
+
+}
+
+/** used mostly for code generation (tabulation via spaces) */
+const TAB = '    ';
